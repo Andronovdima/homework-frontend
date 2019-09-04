17613de8fcc98bc1233f10007d87c08cbffe4f25
Andronovdima <42334635+Andronovdima@users.noreply.github.com>
2019-09-05 01:28:26 +0300

    add jsDoc and right ForEach

--- source/euclid.js
+++ source/euclid.js
@@ -3,20 +3,26 @@
 /**
 * Euclid. Finds GCD
 * @param {number[]} numbers
-* @returns {number}
+* @returns {NOD number}
 */
 const euclid = (...numbers) => {
-	let d = 0;
-	
+	let currentNOD = 0;
+	let isInvalidData = false;
 	numbers.forEach(num => {
-		if (typeof num !== 'number' || typeof d !== 'number') {
-			return "invalid data";
-		}
-		
-		d = euclidForTwo(d , num);
+		if (typeof num !== 'number' || typeof currentNOD !== 'number'){
+			isInvalidData = true;
+			return;
+		}	
+		currentNOD = euclidForTwo(currentNOD , num);
 	});
+	if (isInvalidData)
+		return "invalid data";
+	return currentNOD;
+}
 
-	return d;
-};
-
+/**
+* EuclidForTwo. Finds GCD of two numbers
+* @param {number1, number2}
+* @returns {NOD number}
+*/
 const euclidForTwo = (a, b) => b === 0 ? Math.abs(a) : euclidForTwo(b, a % b);
