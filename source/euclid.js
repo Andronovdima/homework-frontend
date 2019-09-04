223669d872624e933f5ec2573338443d4e1197b2
Andronovdima <42334635+Andronovdima@users.noreply.github.com>
2019-09-04 16:39:47 +0300

    forEach instead of for

--- source/euclid.js
+++ source/euclid.js
@@ -2,12 +2,16 @@
 
 const euclid = (...numbers) => {
 	let d = 0;
-	for (let i=0; i < numbers.length; i++){
-		if (typeof numbers[i] !== 'number' || typeof d !== 'number')
-			return "unvalid data";
-		d = euclidForTwo(d , numbers[i]);
-	}
+	
+	numbers.forEach(num => {
+		if (typeof num !== 'number' || typeof d !== 'number') {
+			return "invalid data";
+		}
+		
+		d = euclidForTwo(d , num);
+	});
+
 	return d;
-}
+};
 
 const euclidForTwo = (a, b) => b === 0 ? Math.abs(a) : euclidForTwo(b, a % b);
