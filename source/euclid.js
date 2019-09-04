28fa6762fd4c29a2110f25425a5509f40f4ccbc2
Andronovdima <42334635+Andronovdima@users.noreply.github.com>
2019-09-04 15:29:11 +0300

    Add files via upload
    Исправил функцию

--- source/euclid.js
+++ source/euclid.js
@@ -1,9 +1,13 @@
 'use strict';
 
-let euclid = function(...numbers) {
+const euclid = (...numbers) => {
 	let d = 0;
-	for (let i=0; i < numbers.length; i++)
+	for (let i=0; i < numbers.length; i++){
+		if (typeof numbers[i] !== 'number' || typeof d !== 'number')
+			return "unvalid data";
 		d = euclidForTwo(d , numbers[i]);
+	}
 	return d;
 }
 
+const euclidForTwo = (a, b) => b === 0 ? Math.abs(a) : euclidForTwo(b, a % b);
