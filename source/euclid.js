c2da709b3626b47696fe2f084324170deb3627ea
Andronovdima <42334635+Andronovdima@users.noreply.github.com>
2019-09-05 11:57:53 +0300

    changed Euclid.js with code Style

--- source/euclid.js
+++ source/euclid.js
@@ -5,18 +5,27 @@
 * @param {number[]} numbers
 * @returns {NOD number}
 */
+
 const euclid = (...numbers) => {
 	let currentNOD = 0;
 	let isInvalidData = false;
+
 	numbers.forEach(num => {
-		if (typeof num !== 'number' || typeof currentNOD !== 'number'){
+		if (
+			typeof num !== 'number' ||
+			typeof currentNOD !== 'number'
+		) {
 			isInvalidData = true;
 			return;
-		}	
+		}
+
 		currentNOD = euclidForTwo(currentNOD , num);
 	});
-	if (isInvalidData)
-		return "invalid data";
+
+	if (isInvalidData) {
+		return 'invalid data';
+	}
+
 	return currentNOD;
 }
 
@@ -25,4 +34,5 @@ const euclid = (...numbers) => {
 * @param {number1, number2}
 * @returns {NOD number}
 */
+
 const euclidForTwo = (a, b) => b === 0 ? Math.abs(a) : euclidForTwo(b, a % b);
